9bc99c566bbc41b15e332657a798b2eb4df0a4f7
Thomas Clark <tnc1997@virginmedia.com>
2020-10-17 19:49:33 +0100

    feat(settings): add path text field

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:train_simulator_client/train_simulator_client.dart';
 import 'package:train_simulator_utilities/src/app.dart';
+import 'package:train_simulator_utilities/src/states/app_state.dart';
 
-void main() {
-  runApp(App());
+Future<void> main() async {
+  TrainSimulatorClient client;
+  final preferences = await SharedPreferences.getInstance();
+  final path = preferences.getString('path');
+  if (path != null) {
+    client = TrainSimulatorClient(
+      options: TrainSimulatorClientOptions(
+        path: path,
+      ),
+    );
+  }
+
+  runApp(
+    AppState(
+      notifier: AppStateData(
+        client: client,
+        path: path,
+      ),
+      child: App(),
+    ),
+  );
 }
