3e944e817c499c8bc80420d6f29e642eccb6ebc5
Thomas Clark <tnc1997@virginmedia.com>
2020-10-24 08:03:54 +0100

    feat(routes): add search

--- lib/src/pages/routes_page.dart
+++ lib/src/pages/routes_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mdi/mdi.dart';
 import 'package:train_simulator_client/train_simulator_client.dart' hide Key;
 import 'package:train_simulator_utilities/src/route_paths/route_route_path.dart';
+import 'package:train_simulator_utilities/src/search_delegates/route_search_delegate.dart';
 import 'package:train_simulator_utilities/src/states/app_state.dart';
 import 'package:train_simulator_utilities/src/states/router_state.dart';
 import 'package:train_simulator_utilities/src/widgets/app_drawer.dart';
@@ -24,6 +26,24 @@ class _RoutesPageState extends State<RoutesPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Routes'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Mdi.magnify),
+            onPressed: () async {
+              final route = await showSearch(
+                context: context,
+                delegate: RouteSearchDelegate(),
+              );
+
+              if (route != null) {
+                RouterState.of(context).path = RouteRoutePath(
+                  id: route.id1.cGuid.devString.toString(),
+                );
+              }
+            },
+            tooltip: 'Search',
+          ),
+        ],
       ),
       body: FutureBuilder<List<CRouteProperties>>(
         future: _future,
